c264f2db96544c00c2a8b1740f136f9947a072d5
Rose <rosevandergiessen@proton.me>
2023-04-17 11:18:49 +0200

    extracted header, main and footer components and their subcomponents

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import LeftMenu from "../Left-menu/Left-menu"
+import RightMenu from "../Right-menu/RightMenu"
+
+function Header() {
+  return(
+    <header>
+      <LeftMenu />
+      <RightMenu />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import Buttons from "../Buttons/Buttons"
+import Logo from "../Logo/Logo"
+import SearchBar from "../Search-bar/SearchBar"
+
+function Main() {
+  return(
+    <main>
+      <Logo />
+      <SearchBar />
+      <Buttons />
+    </main>
+  )
+}
+
+export default Main
